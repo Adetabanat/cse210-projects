2b3cb965bce451188c20d689ff52da8cafa570a9
adetabanat <danieladongonat@gmail.com>
2024-06-07 13:58:09 +0000

    prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade in percentage ");
+        string marks= Console.ReadLine();
+        int grade= int.Parse(marks);
+        
+        if (grade >= 90) 
+        {
+          Console.WriteLine(" You have an A grade.");
+        }
+        else if (grade>=80)
+        {
+             Console.WriteLine(" You have an B grade.");
+        }
+        else if (grade>=70)
+        {
+             Console.WriteLine(" You have an C grade.");
+
+        }
+        else if (grade>=60)
+        {
+             Console.WriteLine(" You have an D grade.");
+        }
+        else if (grade< 50)
+        {
+             Console.WriteLine(" You have an  failed.");
+        }
+    
     }
 }
