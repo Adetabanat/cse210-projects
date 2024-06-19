cf13c148e88307d46cc82b36e471d1884344e74f
Adetabanat <danieladongonat@gmail.com>
2024-06-19 06:31:33 +0100

    class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,7 +2,7 @@
         Job job1 = new Job();
         job1._company = "Apple";
         job1._jobTitle = "Software Developer";
-        job1._startYear = 2015;
+        job1._startYear = 2014;
         job1._endYear = 2019;
 
         Job job2 = new Job();
